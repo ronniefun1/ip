5b8807d48ec2d3105e52318ed199db8b28c60479
Ronnie Fun <ronniefunn@gmail.com>
2025-08-21 13:28:10 +0800

    Complete Level-1

--- src/main/java/Reboot.java
+++ src/main/java/Reboot.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Reboot {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm Reboot\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.println("    ____________________________________________________________\n" +
+                "    Hello! I'm Reboot\n" +
+                "    What can I do for you?\n" +
+                "    ____________________________________________________________\n");
+
+        // Create a Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Get String input
+        String input = scanner.nextLine();
+
+        // Loop when input is not bye
+        while (!input.equals("bye")) {
+            System.out.println("    ____________________________________________________________\n    " +
+                    input +
+                    "\n    ____________________________________________________________\n");
+
+            input = scanner.nextLine();
+        }
+
+        // Close the Scanner
+        scanner.close();
+
+        System.out.println("    ____________________________________________________________\n" +
+                "    Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________\n");
     }
 }
