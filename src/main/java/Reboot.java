8a0d274f7d963a9524cdad30c7b21a453f5233bc
Ronnie Fun <ronniefunn@gmail.com>
2025-08-21 15:20:31 +0800

    Complete ip level-2

--- src/main/java/Reboot.java
+++ src/main/java/Reboot.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Reboot {
     public static void main(String[] args) {
-        System.out.println("    ____________________________________________________________\n" +
+        String solidLn = "    ____________________________________________________________\n";
+
+        System.out.println(solidLn +
                 "    Hello! I'm Reboot\n" +
                 "    What can I do for you?\n" +
-                "    ____________________________________________________________\n");
+                solidLn);
 
         // Create a Scanner
         Scanner scanner = new Scanner(System.in);
@@ -13,20 +15,34 @@ public class Reboot {
         // Get String input
         String input = scanner.nextLine();
 
+        // Initialise tasklist and numTasks
+        String[] tasklist = new String[100];
+        int numTasks = 0;
+
         // Loop when input is not bye
         while (!input.equals("bye")) {
-            System.out.println("    ____________________________________________________________\n    " +
-                    input +
-                    "\n    ____________________________________________________________\n");
-
+            if (input.equals(("list"))) {
+                System.out.print(solidLn);
+                for (int i = 1; i < numTasks + 1; i++) {
+                    System.out.println("    " + i + ". " + tasklist[i - 1]);
+                }
+                System.out.println(solidLn);
+            } else {
+                tasklist[numTasks] = input;
+                numTasks++;
+                System.out.println(solidLn +
+                        "    added: " + input + "\n" +
+                        solidLn);
+            }
             input = scanner.nextLine();
         }
 
         // Close the Scanner
         scanner.close();
 
-        System.out.println("    ____________________________________________________________\n" +
+        System.out.println(solidLn +
                 "    Bye. Hope to see you again soon!\n" +
-                "    ____________________________________________________________\n");
+                solidLn);
+
     }
 }
